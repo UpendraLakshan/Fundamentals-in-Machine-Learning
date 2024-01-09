4fdc16d8cba12c0126539d95b017a3bd364603a4
Upendra De Zoysa <upendradezoysa@gmail.com>
2024-01-09 20:06:46 +0000

    Update 3_KMeans.py

--- 3_KMeans.py
+++ 3_KMeans.py
@@ -6,21 +6,15 @@ from sklearn.preprocessing import scale
 import pandas as pd
 
 bc=load_breast_cancer()
-#print(bc)
 X=scale(bc.data)
 
 y=bc.target 
-#print(X)
-#print(y)
-
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
 model=KMeans(n_clusters=2,random_state=0)
 
 model.fit(X_train)
-
 predictions=model.predict(X_test)
-
 labels=model.labels_
 
 print('Label',labels)
